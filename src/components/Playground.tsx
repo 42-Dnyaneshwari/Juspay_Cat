82827a289ddc928d35879180cf8b0af15f068790
Dnyaneshwari Dolzake <131675072+42-Dnyaneshwari@users.noreply.github.com>
2024-10-20 23:40:29 +0530

    Add cat image

--- src/components/Playground.tsx
+++ src/components/Playground.tsx
@@ -69,11 +69,12 @@ const Playground: React.FC = () => {
           className="relative border-4 border-blue-500 rounded-lg bg-white ml-4"
           style={{ width: boxWidth, height: boxHeight }}
         >
-        <img 
-        src="https://tse4.mm.bing.net/th?id=OIP.UDQELsdJWyTtQA3KBRXjlAHaEj&pid=Api&P=0&h=220" 
-        alt="Cat" 
-        style={{ width: '150px', height: 'auto', borderRadius: '8px' }} 
-        />
+                    <img 
+            src={`${process.env.PUBLIC_URL}/cat.jpg`} 
+            alt="Cat" 
+            style={{ width: '150px', height: 'auto', borderRadius: '8px' }} 
+            />  
+
         </div>
       </div>
     </div>
